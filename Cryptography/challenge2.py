98de9fb748efa8a27bde86291ab7bc599ac5fbf5
John Bedeian <johnbedeian@gmail.com>
2025-09-30 19:10:55 -0400

    challenge2.py

--- Cryptography/challenge2.py
+++ Cryptography/challenge2.py
@@ -1,9 +1,8 @@
 from pwn import xor
 
-xor_string = '686974207468652062756c6c277320657965'          # hex string
-byte_string = bytes.fromhex('1c0111001f010100061a024b53535009181c')  # bytes
+xor_string = bytes.fromhex('686974207468652062756c6c277320657965')
+byte_string = bytes.fromhex('1c0111001f010100061a024b53535009181c')
 
-# Make sure both args are bytes, then call .hex() on the result
-xor_byte_string = xor(byte_string, bytes.fromhex(xor_string))
+xor_byte_string = xor(byte_string, xor_string)
 final_string = xor_byte_string.hex()
-print(final_string)   # -> 746865206b696420646f6e277420706c6179
+print(final_string)
