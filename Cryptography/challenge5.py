3dc3b7ecab3cfd2021516dafaf7a33da4e495728
John Bedeian <johnbedeian@gmail.com>
2025-10-01 15:53:53 -0400

    challenge5.py

--- Cryptography/challenge5.py
+++ Cryptography/challenge5.py
@@ -1,8 +1,8 @@
 phrase = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
 key = "ICE"
 
-res = []
+result = []
 for i in range(len(phrase)):
     res.append(ord(phrase[i]) ^ ord(key[i % len(key)]))
 
-print(bytes(res).hex())
+print(bytes(result).hex())
